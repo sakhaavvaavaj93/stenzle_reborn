1a7d23065357462ae3872ef539251c88aba8f4b4
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2021-08-09 21:32:04 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m stenzle_reborn
+web: python3 -m stenzle_reborn
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE stenzle reborn
+:: Enables virtual env mode and then starts asuna
+env\scripts\activate.bat && py -m stenzle_reborn
